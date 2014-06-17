283a020115cc0a9f5aa991cb96fbb8ea9a7e21c2
a5mith <aaronsmith91@sky.com>
2014-06-17 20:44:08 +0100

    0.0.1
    Much music

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeBB Vexflow Plugin
+
+This NodeBB plugin allows users to embed Vexflow scores inline in posts.
+
+## Installation
+
+    npm install nodebb-plugin-vexflow
+
+## Usage
+
+    Paste the my.vexflow URL into a post, the system will do the rest.
+    ???
+    Profit!
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(module) {
+    "use strict";
+
+    var vexflow = {},
+        embed = '<iframe src="http://static.vexflow.com/embed/$1" width="100%" height="1421" frameborder="0" scrolling="no"></iframe>';
+
+
+    vexflow.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:my?:\/\/)?(?:vexflow\.com)\/?(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
+    module.exports = vexflow;
+}(module));
